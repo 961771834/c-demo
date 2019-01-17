03568d86297765bbf92e451b685419125a12da48
leon.zheng <961771834@qq.com>
2019-01-17 22:57:19 +0800

    update

---
+++ pointer/array-pointer.c
@@ -0,0 +1,13 @@
+/* 
+* 2019年1月14日23:32:28
+* 一维数组和指针的关系
+ */
+
+#include <stdio.h>
+void main(void){
+    int a[5] = {1,2,3,4,5};
+    int i;
+    for(i=0;i<5;i++){
+        printf("%d\n",a[i]);
+    }
+}
