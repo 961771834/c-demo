c7129af98bbf8148722b97a0a34d2ab44adf14a1
leon.zheng <961771834@qq.com>
2019-02-27 23:40:13 +0800

    update

--- struct/student.c
+++ struct/student.c
@@ -7,12 +7,20 @@ struct Student{
 };
 
 void inputStudent(struct Student *stud){
-  stud->age = 12;
-  stud->score=100;
-  stud->sex='F';
+  // stud->age = 12;
+  // stud->score=100;
+  // stud->sex='F';
+  printf("请输入学生的信息:");
+  printf("age =");
+  scanf("%d",&stud->age);
+  printf("score=");
+  scanf("%f",&stud->score);
+  printf("sex=");
+  scanf("%c",&stud->sex);
 };
 
 void outputStudent(struct Student *stud){
+  printf("学生的信息:\n");
   printf("age:%d\n",stud->age);
   printf("score:%f\n",stud->score);
   printf("sex:%c\n",stud->sex);
